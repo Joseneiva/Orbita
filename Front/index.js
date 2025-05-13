7aa2bd8fce64948501a15aced0fbde660d1ab578
Joseneiva <netooliveira012@gmail.com>
2025-05-13 19:28:10 -0300

    Código alterado

--- Front/index.js
+++ Front/index.js
@@ -1,7 +1,23 @@
-fetch('http://localhost:3000/cursos')
-  .then(res => res.json())
-  .then(data => {
-    console.log('Cursos recebidos:', data);
-    // Exibir na tela, por exemplo
-  })
-  .catch(err => console.error('Erro:', err));
+document.addEventListener('DOMContentLoaded', () => {
+  // Seleciona todos os botões com classe .cta-button ou .signup-button
+  const botoes = document.querySelectorAll('.cta-button, .signup-button');
+
+  botoes.forEach(botao => {
+    botao.addEventListener('click', () => {
+      fetch('http://localhost:3000/cta-button', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cursoId: 1 }), // Altere conforme necessário
+      })
+        .then(res => res.json())
+        .then(data => {
+          alert('Inscrição realizada com sucesso!');
+        })
+        .catch(err => {
+          alert('Erro ao realizar inscrição.');
+        });
+    });
+  });
+});
