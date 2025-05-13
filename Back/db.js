1f186b739de51224b38eb9dfd987cedf150b8a85
Joseneiva <netooliveira012@gmail.com>
2025-05-13 18:49:14 -0300

    Remover pasta db

--- Back/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('cursos_EAD', 'root', 'cimatec', {
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306
-});
-
-module.exports = sequelize;
