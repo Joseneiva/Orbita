a9482ae664392ba8979785f0134774b8b5ed9f41
Joseneiva <netooliveira012@gmail.com>
2025-05-14 22:09:31 -0300

    recomeço do projeto limpo

--- Back/Back/index.js
+++
@@ -1,9 +0,0 @@
-const { database } = require('./models');
-
-database.authenticate()
-  .then(() => {
-    console.log('🟢 Conectado com sucesso ao banco de dados.');
-    return database.sync();
-  })
-  .then(() => console.log('🟢 Tabelas sincronizadas.'))
-  .catch(err => console.error('🔴 Erro ao conectar:', err));
